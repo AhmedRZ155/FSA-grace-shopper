34e23ca7514c5dcbae0fc1d8b2b414381ee9f67e
Pichborith Kong <pichborith@gmail.com>
2023-06-23 18:40:36 -0700

    Fixed bugged on boilerplates files

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // ROUTER: /api/users
-const usersRouter = require('./users');
-router.use('/users', usersRouter);
+// const usersRouter = require('./users');
+// router.use('/users', usersRouter);
+
+// ROUTER: /api/products
+// const productsRouter = require('./products');
+// router.use('/products', productsRouter);
+
+module.exports = router;
